31cda1e1c2c529321bdad8d974b71be82d606e94
Kseniia Chumachenko <timeofneon@gmail.com>
2017-11-06 16:34:23 +0200

    Initial commit

---
+++ js/main-c.js
@@ -0,0 +1,34 @@
+'use strict';
+// Create function 'showImages' which
+// loads images.json which has links to images as an array.
+
+// create a loop which builds the following HTML from every image in the array:
+/*
+<li>
+    <figure>
+        <a href="img/original/filename.jpg"><img src="img/thumbs/filename.jpg"></a>
+        <figcaption>
+            <h3>Title</h3>
+        </figcaption>
+    </figure>
+</li>
+*/
+// Make the above HTML by using DOM methods.
+// Create elements with createElement()
+// Add attributes with setAttribute()
+// Add elements with appendChild
+// Add text with createTextNode
+// When the above HTML is ready append it to the <ul> element
+const showImages = () => {
+    const ul = document.querySelector('ul');
+
+    fetch('images.json').then((response) => {
+        return response.json();
+    }).then((json) => {
+        json.forEach((image) => {
+            // put code here
+        });
+    });
+};
+
+showImages();
